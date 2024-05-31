7ece9cfa022b78804b0e9c1ee0eedb9b59699eac
MatheusRodri <matheus.rj25@hotmail.com>
2024-05-31 17:14:33 -0300

    create form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,18 @@
-import { Text, View, StyleSheet } from "react-native"
+import { Text, View, TextInput,TouchableOpacity } from "react-native"
 import {styles} from './styles'
 
 
 export function Home() {
+
+  function handleParticipantAdd(){
+    console.log('Adicionando participante')
+  }
+
+
+
+
+
+
   return (
     <View style={styles.container}>
 
@@ -14,6 +24,21 @@ export function Home() {
         Sábado, 26 de maio de 2024
       </Text>
 
+    <View style={styles.form}>
+    <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor={'#6b6b6b'}
+          keyboardType="default"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+
+    </View>
+      
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,12 +7,40 @@ export const styles = StyleSheet.create({
       padding: 24
     },
     eventName: {
-      color: '#fcfdfe',
+      color: '#fff',
       fontSize: 24,
       fontWeight: "bold",
       marginTop: 48
     },
     eventDate: {
       color: '#6b6b6b',
+    },
+    form:{
+      width:'100%',
+      flexDirection:'row',
+      marginTop:24,
+      marginBottom:24,
+    },
+    input:{
+      backgroundColor:'#1f1e25',
+      height:56,
+      flex:1,
+      borderRadius:8,
+      color:'#fff',
+      padding:16,
+      fontSize:16,
+      marginRight:12,
+    },
+    button:{
+      width:56,
+      height:56,
+      borderRadius:8,
+      backgroundColor:'#31CF67',
+      alignItems:'center',
+      justifyContent:'center',
+    },
+    buttonText:{
+      color:'#fff',
+      fontSize:24,
     }
   });
